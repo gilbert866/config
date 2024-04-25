e77bdfee9a974eda0d664f02229758b8d2542a2a
gilbert866 <56110163+gilbert866@users.noreply.github.com>
2024-04-25 11:43:41 +0000

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- This file is automatically loaded by lazyvim.config.init.
 
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
